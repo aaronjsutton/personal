b309bc79b3836060f1846919f5709ac07658eec4
Aaron Sutton <aaron@aaronjsutton.com>
2020-04-22 23:41:32 -0400

    Add HTML webpack plugin

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env, options) => ({
   optimization: {
@@ -35,6 +36,7 @@ module.exports = (env, options) => ({
     ]
   },
   plugins: [
+    new HtmlWebpackPlugin({filename: "../index.html", template: "static/index.html"}),
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
     new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
   ]

185bdf8f69a5b077730ab0a79f58a9f7007bc04e
Aaron Sutton <aaron@aaronjsutton.com>
2020-04-24 19:08:02 -0400

    Rename to current-nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
 import css from "../css/app.css"
 
 import "phoenix_html"
-import highlight from "./highlight-nav"
+import setCurrent from "./current-nav"
 
 window.addEventListener('DOMContentLoaded', (event) => {
   const nav = document.getElementById('js-nav-highlight')
 
   if (nav) {
-    highlight(nav)
+    setCurrent(nav)
   }
 });

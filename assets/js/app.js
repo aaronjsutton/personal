1238cdb6895c79dc86657bbde80e22be63799275
Aaron Sutton <aaron@aaronjsutton.com>
2020-05-10 22:31:58 -0400

    Add since js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ import css from "../css/app.css"
 
 import "phoenix_html"
 import setCurrent from "./current-nav"
+import since from "./since"
 
 window.addEventListener('DOMContentLoaded', (event) => {
   const nav = document.getElementById('js-nav-highlight')
@@ -9,4 +10,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
   if (nav) {
     setCurrent(nav)
   }
+
+  var elements = document.querySelectorAll('[data-since]')
+  since(elements)
 });
